db5cdbfaf202b2d49e4b86c2982c2714c23c3736
Nikita Tikhonov <tn@stillhuman.com>
2021-08-19 14:25:42 +0300

    Init

---
+++ issues/issue-1/index.test.ts
@@ -0,0 +1,37 @@
+import { isPalindrome } from './index';
+
+test('repaper equal true', () => {
+  expect(isPalindrome('repaper')).toBe(true);
+});
+
+test('malayalam equal true', () => {
+  expect(isPalindrome('malayalam')).toBe(true);
+});
+
+test('I equal true', () => {
+  expect(isPalindrome('I')).toBe(true);
+});
+
+test('bob equal true', () => {
+  expect(isPalindrome(' bob ')).toBe(true);
+});
+
+test('never odd or even equal false', () => {
+  expect(isPalindrome('never odd or even')).toBe(false);
+});
+
+test('procrastination equal false', () => {
+  expect(isPalindrome('procrastination')).toBe(false);
+});
+
+test('string with spaces string equal false', () => {
+  expect(isPalindrome('   ')).toBe(false);
+});
+
+test('empty string equal false', () => {
+  expect(isPalindrome('')).toBe(false);
+});
+
+test('without any agruments equal false', () => {
+  expect(isPalindrome()).toBe(false);
+});
---
+++ issues/issue-1/index.ts
@@ -0,0 +1,17 @@
+/*
+  Palindrome
+*/
+
+export const isPalindrome = (inputString?: string): boolean => {
+  if (typeof inputString !== 'string') {
+    return false;
+  }
+
+  const trimmedString = inputString.trim();
+
+  if (!trimmedString) {
+    return false;
+  }
+
+  return trimmedString === trimmedString.split('').reverse().join('');
+};
